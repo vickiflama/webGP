26d6d28c74b3f3492cfc1365ecfd63c7c968288b
victoria <vickiflama1@gmail.com>
2026-08-19 11:33:48 -0300

    agrego usuario vic

--- admin-config.js
+++ admin-config.js
@@ -1,5 +1,5 @@
 // Lista de usuarios de administración autorizados (agregar acá los nuevos)
-export const ADMIN_USUARIOS = ["usuario8"];
+export const ADMIN_USUARIOS = ["usuario8", "victoria"];
 
 const ADMIN_DOMINIO = "admin.granprix.local";
 
