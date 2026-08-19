8d209a1519745a3a4e359d805f6177fd7db013ed
victoria <vickiflama1@gmail.com>
2026-08-19 12:26:49 -0300

    cambios en registro de admin

--- admin-config.js
+++ admin-config.js
@@ -1,5 +1,5 @@
 // Lista de usuarios de administración autorizados (agregar acá los nuevos)
-export const ADMIN_USUARIOS = ["usuario8", "victoria"];
+export const ADMIN_USUARIOS = ["victoria", "usuario8"];
 
 const ADMIN_DOMINIO = "admin.granprix.local";
 
